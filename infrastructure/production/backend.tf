4c7c68a1bb14786dd590cf4ad8c8517d019547cb
juane <juaneste687@gmail.com>
2026-05-03 13:46:45 -0500

    fix: full deploy

--- infrastructure/production/backend.tf
+++ infrastructure/production/backend.tf
@@ -316,7 +316,7 @@ resource "aws_security_group" "alb" {
 }
 
 resource "aws_lb_target_group" "backend" {
-  name     = "horrocruxes-backend-${var.environment}"
+  name     = "horrocruxes-backend-${var.environment}-tg"
   port     = 8080
   protocol = "HTTP"
   vpc_id   = data.aws_vpc.default.id
