578f591597670eaec90a346f89fcd4ce4cd09cf9
juane <juaneste687@gmail.com>
2026-05-03 14:55:46 -0500

    fix: full deploy

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <div className="header">
-      <p className="magic-subtitle">Wizarding Knowledge System #2</p>
+      <p className="magic-subtitle">Wizarding Knowledge System #3</p>
       <h1>HORROCRUXES</h1>
       <p className="header-description">
         Explore the universe of Harry Potter with the help of an intelligent question and answer system.
