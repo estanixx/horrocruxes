4762c0608c2f6985cb615fddc525858318baf76c
juane <juaneste687@gmail.com>
2026-05-03 14:38:30 -0500

    fix: full deploy

--- infrastructure/production/backend.tf
+++ infrastructure/production/backend.tf
@@ -245,13 +245,15 @@ resource "aws_security_group" "ecs_tasks" {
   description = "Security group for ECS tasks"
   vpc_id      = data.aws_vpc.default.id
   
+  # Allow traffic from ALB on container port
   ingress {
-    from_port   = 8000
-    to_port     = 8000
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.alb.id]
   }
   
+  # Allow all outbound
   egress {
     from_port   = 0
     to_port     = 0
